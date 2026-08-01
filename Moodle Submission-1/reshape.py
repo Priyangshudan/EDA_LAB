298181072b80263c8a581bfee0d1e123c6639322
Priyangshudan <priyangshudan@gmail.com>
2026-08-01 17:47:22 +0530

    Made changes to reshape.py

--- Moodle Submission-1/reshape.py
+++ Moodle Submission-1/reshape.py
@@ -12,9 +12,15 @@ dframe1 = pd.DataFrame(
 )
 
 print(dframe1)
+print("----------\n")
+
 stacked = dframe1.stack()
 print(stacked)
+print("----------\n")
+
 print(stacked.unstack())
+print("----------\n")
+
 
 series1 = pd.Series(
     [10, 20, 30, 40],
@@ -29,4 +35,7 @@ series2 = pd.Series(
 frame2 = pd.concat([series1, series2], keys=['GroupA', 'GroupB'])
 
 print(frame2)
+print("----------\n")
+
 print(frame2.unstack())
+print("----------\n")
